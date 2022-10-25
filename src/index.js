561639d0b2b7229428c12fe54ebdf289b03f11cc
KelwinGerson <83376362+KelwinGerson@users.noreply.github.com>
2022-10-25 06:40:24 -0300

    feat: Create a user + check if cpf already exists.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,43 @@
 const express = require('express')
+const {v4: uuidv4 } = require('uuid')
+// uuid v4 utiliza números randomicos
 
 const app = express()
 
-// cpf - String
-// name - String
-// id - uuid
-// statement - []
+const customers = [];
+
+// middleware
+app.use(express.json());
 
 app.post('/account', (request, response) => {
     // desestruturação
-    const {cpf, name} = request.body;
-    
-    
-})
+    const { cpf, name } = request.body;
 
-app.listen(1234);
+    const customerAlreadyExists = customers.some(
+        // (===) checks whether its two operands are equal, returning a Boolean result 
+        (customer) => customer.cpf === cpf
+    );
+
+    if (customerAlreadyExists) {
+        return response.status(400).json({
+            error: "Customer already exists!"
+        })
+    }
+
+
+    customers.push({
+        cpf,
+        name,
+        id: uuidv4(),
+        statement: []
+    });
+
+    return response.status(201).send({
+        message: "Usuário criado com sucesso"
+    })
+    
+});
+
+app.listen(1234, () => {
+    console.log('Listening API...')
+});
