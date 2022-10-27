5143c2e0b5872bc6baca336114e8c90d293c777a
KelwinGerson <83376362+KelwinGerson@users.noreply.github.com>
2022-10-27 08:06:08 -0300

    feat: delete and get balance

--- src/index.js
+++ src/index.js
@@ -148,6 +148,24 @@ app.get("/account", verifyIfExistsAccountCPF, (request, response) => {
     return response.json(customer)
 });
 
+app.delete("/account", verifyIfExistsAccountCPF, (request, response) => {
+    const { customer } = request;
+
+    // splice get array and delete last one
+    customers.splice(customer, 1);
+
+    return response.status(200).json(customers);
+});
+
+
+app.get("/balance", verifyIfExistsAccountCPF, (request, response) => {
+    const { customer } = request;
+
+    const balance = getBalance(customer.statement)
+
+    return response.json(balance)
+});
+
 app.listen(1234, () => {
     console.log('Listening API...')
 });
